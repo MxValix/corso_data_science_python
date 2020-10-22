556f57701cd6de62cb14d73143c54338933aa9ac
Mx. Valix <65033636+MxValix@users.noreply.github.com>
2020-10-22 14:52:48 +0200

    Update scrittura_testo.py

--- scrittura_testo.py
+++ scrittura_testo.py
@@ -1,4 +1,21 @@
-if __name__ == '__main__':
+def apertura_file():
     with open("text.txt", mode="a") as f:
         t = f.write("prova di scrittura")
         print(f)
+
+
+#dizionario con chiave parola e valore numero occorrenze
+def numero_occorrenze(key):
+    frase = "Ma in Italia, o dolce Italia, la gente è più sincera, la vita è più vera."
+    s = frase.split(key)
+    dim = len(s)-1
+    return dim
+
+
+if __name__ == '__main__':
+    print("Inserisci la parola da ricercare: ")
+    a = input()
+    n = numero_occorrenze(a)
+    print("Ci sono", n, "occorrenze della parola", a)
+    #apertura_file()
+
