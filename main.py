afb2ae0aa4119bc4dcdc1bf7f25dbf23ed9f0842
Mx. Valix <65033636+MxValix@users.noreply.github.com>
2020-10-22 11:53:29 +0200

    Pari Dispari e divisore

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ def is_even(n)->int:
         return False
 
 
+def is_div(a, b):
+    return a % b == 0
 
 if __name__ == '__main__':
-    is_even = is_even(int(input()))
+    print("inserisci il numero di cui vuoi sapere se è pari o dispari: ")
+    a = int(input());
+    is_even = is_even(a)
     response = "il numero è "
     if (is_even):
-        response += "pari"
+        response += "pari e "
     else:
-        response += "dispari"
-    print(response)
+        response += "dispari e "
+    print("inserisci il divisore: ")
+    b = int(input())
+    is_div = is_div(a, b)
+    if (is_div):
+        response += "divisibile per "
+    else:
+        response += "non è divisibile per "
+    print(response, b)
