474e222bc6ac1b94f806d4d1344e9e5237b831f8
Mx. Valix <65033636+MxValix@users.noreply.github.com>
2020-10-22 15:21:57 +0200

    Update scrittura_testo.py

--- scrittura_testo.py
+++ scrittura_testo.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
 def apertura_file():
     with open("text.txt", mode="a") as f:
         t = f.write("prova di scrittura")
@@ -12,10 +15,10 @@ def numero_occorrenze(key):
     return dim
 
 
-def lettura_file():
-    with open("text.txt") as f:
+def lettura_file(nome_file, replace, replace_with):
+    with open(nome_file) as f:
         str = f.read()
-    s = str.replace(",", " ").split(" ")
+    s = str.replace(replace, replace_with).split(replace_with)
     return s
 
 
@@ -29,11 +32,26 @@ def crea_dizionario(s):
     return dizionario
 
 
+def open_csv(nome_file, dizionario):
+    with open(nome_file, mode="w") as f:
+        #f.write("parola,conteggio")
+        for parola in dizionario.keys():
+            f.write(str(parola) + "," + str(dizionario[parola]) + "\n")
+
+
 if __name__ == '__main__':
     # print("Inserisci la parola da ricercare: ")
     # a = input()
     # n = numero_occorrenze(a)
     # print("Ci sono", n, "occorrenze della parola", a)
     #apertura_file()
-    s = lettura_file()
-    print(crea_dizionario(s))
+    nome_file = "addresses.csv"
+    replace = ","
+    replace_with = " "
+    s = lettura_file(nome_file, replace, replace_with)
+    dizionario = crea_dizionario(s)
+    nome_file = "parola.csv"
+    open_csv(nome_file,dizionario)
+
+    df = pd.read_csv(nome_file)
+    print(df[""])
