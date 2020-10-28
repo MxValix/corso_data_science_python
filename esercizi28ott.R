5a1368db78a9fbb7ea0b32c8b8b73b7b4cde4df3
Mx. Valix <65033636+MxValix@users.noreply.github.com>
2020-10-28 15:51:02 +0100

    Update esercizi28ott.R

--- esercizi28ott.R
+++ esercizi28ott.R
@@ -1,5 +1,15 @@
 # Esercizio 1 #######
 
+is_prime <- function(num) {
+  if (num == 2) {
+    TRUE
+  } else if (any(num %% 2:(num-1) == 0)) {
+    FALSE
+  } else { 
+    TRUE
+  }
+}
+
 x <- readline(prompt="Inserisci un intero x: ")
 x <- as.integer(x)
 
@@ -9,4 +19,9 @@ if (x%%2 == 0) {
   print("È pari")
 } else {
   print("È dispari")
+}
+if (is_prime(x)){
+  print("è primo")
+} else {
+  print("non è primo")
 }
