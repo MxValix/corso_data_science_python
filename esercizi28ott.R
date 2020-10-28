9cc45a7a7104479b73f172cefa6d549b99435e1d
Mx. Valix <65033636+MxValix@users.noreply.github.com>
2020-10-28 16:06:04 +0100

    Update esercizi28ott.R

--- esercizi28ott.R
+++ esercizi28ott.R
@@ -13,7 +13,7 @@ is_prime <- function(num) {
 x <- readline(prompt="Inserisci un intero x: ")
 x <- as.integer(x)
 
-ifelse(x %% 2 == 0,"È pari","È dispari")
+#ifelse(x %% 2 == 0,"È pari","È dispari")
 
 if (x%%2 == 0) {
   print("È pari")
