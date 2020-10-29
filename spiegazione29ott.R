1611fb19697d7368b60509e45c9864cd2bb965cd
Mx. Valix <65033636+MxValix@users.noreply.github.com>
2020-10-29 10:19:27 +0100

    Update spiegazione29ott.R

--- spiegazione29ott.R
+++ spiegazione29ott.R
@@ -1,3 +1,5 @@
+# DATAFRAME ####
+
 v1 <- c(1,2,3)
 v2 <- c("a")
 v3 <- c(T,F,T)
@@ -15,3 +17,49 @@ rownames(DS) <- c("rowA", "rowB", "rowC")
 names(DS) <- c("colA", "colB", "colC", "colD", "colE")
 
 DS[c("rowA"),c("colC")]
+
+# LIST ####
+
+o1 <- c(1,2,3)
+o2 <- c("a","b","c","d")
+o3 <- c(T,F,T,T,F)
+
+list1 <- list(o1,o2,o3) 
+View(list1)
+
+list2 <- list(o1,o2,o2,list1)
+View(list2)
+
+list1[1]
+list2[4]
+
+names(list1) <- c("first","second","third")
+list1
+
+View(list1)
+
+list1[c("second")]
+list1$second
+str(list1)
+typeof(list1$third)
+
+movies <- c("SPIDERMAN", "BATMAN", "VERTIGO", "CHINATOWN")
+movies_lower <- lapply(movies,tolower)
+str(movies_lower)
+View(movies_lower)
+
+# SAPPLY ####
+data(movies)
+View (movies)
+
+dt <- movies
+sum_movies <- sapply(dt,min)
+sum_movies
+sum_movies <- sapply(dt,max)
+sum_movies
+
+lmn_movies <- lapply(dt,min)
+lmn_movies
+
+lmx_movies <- lapply(dt,max)
+lmx_movies
