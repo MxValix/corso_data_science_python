001c85fde4bae273a48e065d2774a5f34063ab07
Mx. Valix <65033636+MxValix@users.noreply.github.com>
2020-10-29 16:47:37 +0100

    Update esercizi28ott.R

--- esercizi28ott.R
+++ esercizi28ott.R
@@ -1,4 +1,7 @@
-# Esercizio 1 #######
+# Esercizio 1 ####
+
+x <- readline(prompt="Inserisci un intero x: ")
+x <- as.integer(x)
 
 is_prime <- function(num) {
   if (num == 2) {
@@ -9,10 +12,6 @@ is_prime <- function(num) {
     TRUE
   }
 }
-
-x <- readline(prompt="Inserisci un intero x: ")
-x <- as.integer(x)
-
 #ifelse(x %% 2 == 0,"È pari","È dispari")
 
 if (x%%2 == 0) {
@@ -31,7 +30,8 @@ if (is_prime(x)){
   print("non è primo")
 }
 
-# Esercizio 2
+
+# Esercizio 2 ####
 
 cnt <- 1
 
@@ -42,7 +42,7 @@ while (cnt < 10) {
 }
 
 
-# Esercizio 3
+# Esercizio 3 ####
 
 v <- seq(1,20,by=2)
 print(v)
@@ -53,15 +53,13 @@ for (x in v){
 }
 
 
-# Esercizio 4
-
+# Esercizio 4 ####
 
 x <- 0
 while (x<=5) {
-  x <- readline(prompt="Inserisci un numero maggiore di 5: ")
-  x <- as.integer(x)
+  x <- as.integer(readline(prompt="Inserisci un numero maggiore di 5: "))
 }
-matrice <- matrix(1:x, nrow = x, ncol = x)
+matrice <- matrix(0, nrow = x, ncol = x)
 for (i in 1:x){
   for (j in 1:x){
     prod <- i*j
@@ -71,7 +69,33 @@ for (i in 1:x){
 View(matrice)
 
 
-# Esercizio 5
+# Esercizio 5 ####
 
 sottomatrice <- matrice[0:5,0:5]
 View (sottomatrice)
+
+
+# Esercizio Data Structure 1 ####
+
+# genero i numeri random e matrice 15x7
+righe <- 15
+colonne <- 7
+rand <- sample(1:1000, righe*colonne)
+matriceRandom <- matrix(rand, nrow = righe, ncol = colonne)
+View(matriceRandom)
+# estrapolo la colonna 1 e 3 insieme
+df_matriceRandom <- matriceRandom[,c(1,3)]
+View(df_matriceRandom)
+# seleziono tutti gli elementi < 20 dalla colonna 1
+df_sottomatrice <- df_matriceRandom[which(df_matriceRandom[,1]<20), select=1]
+View(df_sottomatrice)
+# eseguo la somma delle colonne e le inserisco in un vettore
+sommaColonne <- colSums(matriceRandom)
+sommaColonne
+length(sommaColonne)
+# sottomatrice con valori < 100
+sottomatrice100 = matriceRandom[which(matriceRandom>100)]
+View(sottomatrice100)
+# diagonale sottomatrice 7x7
+sottomatrice7x7 <- diag(matriceRandom[0:7,0:7])
+View(sottomatrice7x7)
