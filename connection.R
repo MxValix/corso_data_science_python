8b967258251594982818c8c114b153984c960b95
Mx. Valix <65033636+MxValix@users.noreply.github.com>
2020-11-03 09:42:26 +0100

    Create connection.R

---
+++ connection.R
@@ -0,0 +1,12 @@
+library(DBI)
+library(RPostgreSQL)
+library(odbc)
+db_password <- "admin"
+drv <- dbDriver("PostgreSQL")
+connessione <- dbConnect( drv, 
+  dbname='textmining', 
+  host='localhost', 
+  port=5432, 
+  user = 'postgres',
+  password=  db_password 
+)
