43e04a55929df75ee26f41c814537d3ff5ab859e
Mx. Valix <65033636+MxValix@users.noreply.github.com>
2020-11-03 17:40:12 +0100

    Update primo.R

--- primo.R
+++ primo.R
@@ -12,20 +12,25 @@ connessione <- dbConnect( drv,
 dbWriteTable(connessione, "WomenClothing", textmining)
 data <- dbReadTable(connessione, "WomenClothing")
 View(data)
+#boh <- subset(data, Rating < 3)
+#View(boh)
 distinct_id <- unique(data$Clothing.ID)
 d_len <- length(distinct_id)
 View(distinct_id)
 tmp <- data.frame(Clothing.ID = numeric())
-distinct <- data.frame(Clothing.ID = numeric(), Rating = numeric())
+distinct <- data.frame(Clothing.ID = numeric(), Rating = numeric(), Eta = numeric())
 
 for (d in distinct_id){
   tmp <- subset(data, Clothing.ID == d)
-  media <- mean(tmp$Rating)
-  distinct <- rbind(distinct,data.frame(tmp$Clothing.ID,media))
+  mediaRating <- mean(tmp$Rating)
+  mediaAge <- mean(tmp$Age)
+  distinct <- rbind(distinct,data.frame(tmp$Clothing.ID,mediaRating,mediaAge))
 }
 distinct <- unique(distinct)
 names(distinct)[1] <- "Clothing.ID"
 names(distinct)[2] <- "Rating"
+names(distinct)[3] <- "Age"
+distinct <- distinct[order(distinct[2]),]
 dbWriteTable(connessione,"distinct", distinct)
 distinct <- dbReadTable(connessione,"distinct")
 View(distinct)
